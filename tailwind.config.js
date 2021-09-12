9e02785779d499a8c8dbaf5294fd3da3223df676
Jose Paz <jpaz17@hotmail.com>
2021-09-12 19:06:22 -0300

    💄 Purga de estilos

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    "./docs/*.html"
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
+}
