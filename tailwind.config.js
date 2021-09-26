a3945ba7127063079d09312b644fd4cbf74c718e
Jose Paz <jpaz17@hotmail.com>
2021-09-26 16:25:45 -0300

    📱Agregadas clases responsive e implementación

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 module.exports = {
   purge: [
-    "./docs/*.html"
+    "docs/*.html", "docs/*.js"
   ],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
+    screens: {
+      'phone': '320px',
+      'tablet': '640px',
+      'laptop': '1024px',
+      'desktop': '1280px'
+    }
   },
   variants: {
+    button: ['responsive', 'hover', 'active'],
     extend: {},
   },
   plugins: [require('@tailwindcss/typography')],
